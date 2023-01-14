7ac79795a33929d5f2bfc3cd838646109866a1ca
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-01-14 21:54:08 +0300

    update

--- DesignPatterns/WebApp.Strategy/Controllers/ProductsController.cs
+++ DesignPatterns/WebApp.Strategy/Controllers/ProductsController.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BaseProject.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using BaseProject.Models;
 using WebApp.Strategy.Models;
 
 namespace WebApp.Strategy.Controllers
